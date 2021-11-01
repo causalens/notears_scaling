dda6de55cd141891b0e76562165917d3492cf71c
marcuskaiser-cl <44672548+marcuskaiser-cl@users.noreply.github.com>
2021-11-01 22:24:53 +0000

    Add files via upload

---
+++ notears_scaling.py
@@ -0,0 +1,215 @@
+import igraph as ig
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.linalg as slin
+import scipy.optimize as sopt
+from scipy.special import expit as sigmoid
+
+
+def is_dag(W):
+    G = ig.Graph.Weighted_Adjacency(W.tolist())
+    return G.is_dag()
+
+
+def simulate_linear_sem(W, n, sem_type, noise_scale=None):
+    """Simulate samples from linear SEM with specified type of noise.
+
+    For uniform, noise z ~ uniform(-a, a), where a = noise_scale.
+
+    Args:
+        W (np.ndarray): [d, d] weighted adj matrix of DAG
+        n (int): num of samples, n=inf mimics population risk
+        sem_type (str): gauss, exp, gumbel, uniform, logistic, poisson
+        noise_scale (np.ndarray): scale parameter of additive noise, default all ones
+
+    Returns:
+        X (np.ndarray): [n, d] sample matrix, [d, d] if n=inf
+    """
+
+    def _simulate_single_equation(X, w, scale):
+        """X: [n, num of parents], w: [num of parents], x: [n]"""
+        if sem_type == 'gauss':
+            z = np.random.normal(scale=scale, size=n)
+            x = X @ w + z
+        elif sem_type == 'exp':
+            z = np.random.exponential(scale=scale, size=n)
+            x = X @ w + z
+        elif sem_type == 'gumbel':
+            z = np.random.gumbel(scale=scale, size=n)
+            x = X @ w + z
+        elif sem_type == 'uniform':
+            z = np.random.uniform(low=-scale, high=scale, size=n)
+            x = X @ w + z
+        elif sem_type == 'logistic':
+            x = np.random.binomial(1, sigmoid(X @ w)) * 1.0
+        elif sem_type == 'poisson':
+            x = np.random.poisson(np.exp(X @ w)) * 1.0
+        else:
+            raise ValueError('unknown sem type')
+        return x
+
+    d = W.shape[0]
+    if noise_scale is None:
+        scale_vec = np.ones(d)
+    elif np.isscalar(noise_scale):
+        scale_vec = noise_scale * np.ones(d)
+    else:
+        if len(noise_scale) != d:
+            raise ValueError('noise scale must be a scalar or has length d')
+        scale_vec = noise_scale
+    if not is_dag(W):
+        raise ValueError('W must be a DAG')
+    if np.isinf(n):  # population risk for linear gauss SEM
+        if sem_type == 'gauss':
+            # make 1/d X'X = true cov
+            X = np.sqrt(d) * np.diag(scale_vec) @ np.linalg.inv(np.eye(d) - W)
+            return X
+        else:
+            raise ValueError('population risk not available')
+    # empirical risk
+    G = ig.Graph.Weighted_Adjacency(W.tolist())
+    ordered_vertices = G.topological_sorting()
+    assert len(ordered_vertices) == d
+    X = np.zeros([n, d])
+    for j in ordered_vertices:
+        parents = G.neighbors(j, mode=ig.IN)
+        X[:, j] = _simulate_single_equation(X[:, parents], W[parents, j], scale_vec[j])
+    return X
+
+
+
+def gradient(f, w, eps=1e-6):
+    out = np.zeros_like(w)
+    e = np.zeros_like(w)
+    for i in range(w.shape[0]):
+        for j in range(w.shape[1]):
+            e[i, j] = eps
+            out[i, j] = (f(w + e) - f(w - e)) / (2 * eps)
+            e[i, j] = 0.0
+    return np.array(out)
+
+
+def notears_linear(X, lambda1, loss_type, max_iter=100, h_tol=1e-8, rho_max=1e+16, w_threshold=0.3):
+    """Solve min_W L(W; X) + lambda1 ‖W‖_1 s.t. h(W) = 0 using augmented Lagrangian.
+
+    Args:
+        X (np.ndarray): [n, d] sample matrix
+        lambda1 (float): l1 penalty parameter
+        loss_type (str): l2, logistic, poisson
+        max_iter (int): max num of dual ascent steps
+        h_tol (float): exit if |h(w_est)| <= htol
+        rho_max (float): exit if rho >= rho_max
+        w_threshold (float): drop edge if |weight| < threshold
+
+    Returns:
+        W_est (np.ndarray): [d, d] estimated DAG
+    """
+
+    def _loss(W):
+        """Evaluate value and gradient of loss."""
+        M = X @ W
+        if loss_type == 'l2':
+            R = X - M
+            loss = 0.5 / X.shape[0] * (R ** 2).sum()
+            G_loss = - 1.0 / X.shape[0] * X.T @ R
+        elif loss_type == 'logistic':
+            loss = 1.0 / X.shape[0] * (np.logaddexp(0, M) - X * M).sum()
+            G_loss = 1.0 / X.shape[0] * X.T @ (sigmoid(M) - X)
+        elif loss_type == 'poisson':
+            S = np.exp(M)
+            loss = 1.0 / X.shape[0] * (S - X * M).sum()
+            G_loss = 1.0 / X.shape[0] * X.T @ (S - X)
+        elif loss_type == 'golem_nv':
+            # REMARK: This is the main modification of the code. We added a new loss function
+            # which allows to reproduce the GOLEM-NV method from https://arxiv.org/abs/2104.05441:
+            loss_fn = lambda W: 0.5 * np.log(((X - X @ W) ** 2).sum(axis=0)).sum() \
+                                - np.linalg.slogdet(np.eye(W.shape[0]) - W)[1]
+            loss = loss_fn(W)
+            G_loss = gradient(loss_fn, W)
+        else:
+            raise ValueError('unknown loss type')
+        return loss, G_loss
+
+    def _h(W):
+        """Evaluate value and gradient of acyclicity constraint."""
+        E = slin.expm(W * W)  # (Zheng et al. 2018)
+        h = np.trace(E) - d
+        #     # A different formulation, slightly faster at the cost of numerical stability
+        #     M = np.eye(d) + W * W / d  # (Yu et al. 2019)
+        #     E = np.linalg.matrix_power(M, d - 1)
+        #     h = (E.T * M).sum() - d
+        G_h = E.T * W * 2
+        return h, G_h
+
+    def _adj(w):
+        """Convert doubled variables ([2 d^2] array) back to original variables ([d, d] matrix)."""
+        return (w[:d * d] - w[d * d:]).reshape([d, d])
+
+    def _func(w):
+        """Evaluate value and gradient of augmented Lagrangian for doubled variables ([2 d^2] array)."""
+        W = _adj(w)
+        loss, G_loss = _loss(W)
+        h, G_h = _h(W)
+        obj = loss + 0.5 * rho * h * h + alpha * h + lambda1 * w.sum()
+        G_smooth = G_loss + (rho * h + alpha) * G_h
+        g_obj = np.concatenate((G_smooth + lambda1, - G_smooth + lambda1), axis=None)
+        return obj, g_obj
+
+    n, d = X.shape
+    w_est, rho, alpha, h = np.zeros(2 * d * d), 1.0, 0.0, np.inf  # double w_est into (w_pos, w_neg)
+    bnds = [(0, 0) if i == j else (0, None) for _ in range(2) for i in range(d) for j in range(d)]
+    if loss_type == 'l2':
+        X = X - np.mean(X, axis=0, keepdims=True)
+    for _ in range(max_iter):
+        w_new, h_new = None, None
+        while rho < rho_max:
+            sol = sopt.minimize(_func, w_est, method='L-BFGS-B', jac=True, bounds=bnds)
+            w_new = sol.x
+            h_new, _ = _h(_adj(w_new))
+            if h_new > 0.25 * h:
+                rho *= 10
+            else:
+                break
+        w_est, h = w_new, h_new
+        alpha += rho * h
+        if h <= h_tol or rho >= rho_max:
+            break
+    W_est = _adj(w_est)
+    W_est[np.abs(W_est) < w_threshold] = 0
+    return W_est
+
+
+if __name__ == '__main__':
+    np.random.seed(1)
+
+    # Create a simple model problem.
+    W_true = np.array([
+        [0.0, 1.7, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 0.0],
+        [0.0, 0.6, 0.0, 0.0],
+        [-0.8, -1.4, -1.7, 0.0],
+    ])
+    B_true = np.abs(W_true) > 1e-15
+    X = simulate_linear_sem(W_true, n=10000, sem_type='gauss')
+
+    # Setup hyperparameters:
+    lambda1 = 0.01
+    loss_notears = 'l2'
+    loss_golem_nv = 'golem_nv'
+
+    # Run experiments for the first time:
+    W_est = notears_linear(X, lambda1=lambda1, loss_type=loss_notears)
+    print(W_est)
+
+    W_est = notears_linear(X, lambda1=lambda1, loss_type=loss_golem_nv)
+    print(W_est)
+
+    # Rescale the last variable by a factor of 3:
+    X[:, 3] *= 10
+
+    # Run experiment again. The result changes in both cases.
+    W_est = notears_linear(X, lambda1=lambda1, loss_type=loss_notears)
+    print(W_est)
+
+    W_est = notears_linear(X, lambda1=lambda1, loss_type=loss_golem_nv)
+    print(W_est)
